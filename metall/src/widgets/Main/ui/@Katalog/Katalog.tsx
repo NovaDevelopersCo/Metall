4ed4c95d15c98d6d45ccf2c8a5dbad60c89ffaca
Yaroslavl1s <lisinskijaroslav@gmail.com>
2024-05-08 13:31:06 +0300

    add foto , text , Card

---
+++ metall/src/widgets/Main/ui/@Katalog/Katalog.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Katalog = () => {
+  return (
+    <div>Katalog</div>
+  )
+}
--- metall/src/widgets/Main/ui/Main.tsx
+++ metall/src/widgets/Main/ui/Main.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 
-import styles from './Main.module.scss';
+import Card from './@Card/Card';
+import { Katalog } from './@Katalog/Katalog';
+
+import style from './Main.module.scss';
 
 const Main = () => {
   return (
-    <div className={styles.container}>
-      <img className={styles.ImgMetall}src="./assets/metallchell1.png" alt="Metallchell1" />
-      <div>
-        <h3 className={styles.Text}>Металлопрокат высочайшего качества</h3>
+    <div>
+      <div className={style.container}>
+        <img className={style.ImgMetall} src="./assets/metallchell1.png" alt="Metallchell1" />
+        <div className={style.contTxt}>
+          <button className={style.button}>Заказать звонок</button>
+          <h3 className={style.Text}>Металлопрокат высочайшего качества</h3>
+          <h2 className={style.H2Text}>Надежные поставки, оперативная доставка и гарантированное качество для вашего успешного проекта. Работаем оптом и в розницу.</h2>
+        </div>
       </div>
-      <div>
-        <h2 className={styles.H2Text}>Надежные поставки, оперативная доставка и гарантированное качество для вашего успешного проекта. Работаем оптом и в розницу.</h2>     
-      </div>             
-    </div>  
+      <div className={style.Cont}>
+        <div className={style.Card1}>
+          <Card icon="/icon/image11.png" title="Быстрая доставка" text="Гарантируем оперативную доставку металлопроката" />
+        </div>
+        <div className={style.Card2}>
+          <Card icon="/icon/image22.png" title="Профессиональная консультация" text="Мы помогаем в выборе оптимальных решений" />
+        </div>
+        <div className={style.Card3}>
+          <Card icon="/icon/image33.png" title="Высокое качество" text="Соответствующего всем необходимым стандартам" />
+        </div>
+      </div>
+      <Katalog />
+    </div>
   );
 };
 
