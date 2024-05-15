cd2202b562b0baf67a1f94782e713a3737de864e
Igainst <antimagdota2@mial.ru>
2024-05-15 09:07:11 +0300

    AboutUs

--- metall/src/abotUs/AboutUs.buttons.tsx
+++ metall/src/abotUs/AboutUs.buttons.tsx
@@ -5,8 +5,8 @@ import {AboutUsButtons} from './AboutUs.data'
 export const AboutUsbuttonsItems = ({}) =>{
     return (
             <div className="flex justify-center my-[90px]">
-                <button className="bg-yellow-400 text-black w-[221px] rounded-md  h-[55px]  m-[8px] shadow-xl">Заказать звонок</button>
-                <button className="bg-white-400 text-black border-black border-2 w-[221px] rounded-md h-[55px]  m-[8px]">Оставить заявку</button>
+                <button className="bg-yellow-400 text-black w-[221px] rounded-md  h-[55px]  m-[8px] shadow-xl font-roboto font-bold">Заказать звонок</button>
+                <button className="bg-white-400 text-black border-black border-2 w-[221px] rounded-md h-[55px]  m-[8px] font-roboto font-bold">Оставить заявку</button>
             </div>
  
     )
