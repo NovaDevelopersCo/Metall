9973487af04338982e217d0839b7942f7ee90dec
cvccvc40 <ebgs3534@gmail.com>
2024-05-31 20:50:04 +0300

    Changed maps to Google Maps and fixed the adaptive design

--- metall/src/widgets/Contacts/ui/MapLocation/MapLocation.tsx
+++ metall/src/widgets/Contacts/ui/MapLocation/MapLocation.tsx
@@ -1,9 +1,11 @@
 
+import styles from './MapLocation.module.scss'
+
 const MapLocation: React.FC = () => {
     return (
-        <>
-            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af817fc3de7f02dcd72d262cf709abb57369a6e9950ecf567f215dc24bcb4c84f&amp;source=constructor" width="100%" height="582" frameBorder="0"></iframe>
-        </>
+        <section className={styles.MapContainer}>
+           <iframe className={styles.MapIframe} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d577343.9845137314!2d36.72620264522804!3d55.58025707094451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afc73d4b0c9%3A0x3d44d6cc5757cf4c!2z0JzQvtGB0LrQstCwLCDQoNC-0YHRgdC40Y8!5e0!3m2!1sru!2s!4v1716811930788!5m2!1sru!2s" loading="lazy"></iframe>
+        </section>
     )
 }
 
