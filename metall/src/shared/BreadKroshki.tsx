3f202f3eda835d08a751b582a2cc4626dd323255
outsided <antimagdota2@mail.ru>
2024-06-10 20:54:52 +0300

    link

--- metall/src/shared/BreadKroshki.tsx
+++ metall/src/shared/BreadKroshki.tsx
@@ -1,19 +1,18 @@
+import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { useEffect } from "react"
 
 export const BreadKroshki = () =>{
     const rout = usePathname().split('/')
     useEffect(()=>{},[rout])    
-  
+
+
+
     return (
-        <>
-        <div className="flex flex-row">
-        <div className="w-[200px] text-roboto text-[18px] font-normal">Главная <span  className="mx-[12px]">—</span>
-            {
-              <span className="text-yellow-400">{rout.map(href=> href==='aboutUs'?href:'')}</span>
-            }
-            </div>
-        </div>
-        </>
+               <div className="flex w-[200px] text-roboto text-[18px] font-normal "><Link href='/' className="cursor-pointer">Главная</Link><span  className="mx-[12px]">—</span>
+               {
+                 <span className="text-yellow-400">{rout.map(href=> href==='aboutUs'?'О нас':'')}</span>
+                }
+              </div>        
         )
 }
